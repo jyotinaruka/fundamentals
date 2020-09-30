3c3024a2c487eb99134a9101687b218ebb6c137f
Jyoti Singh <jn.naruka@gmail.com>
2020-09-30 13:27:20 -0700

    Python Fundamentals

---
+++ function_intermediate_I.py
@@ -0,0 +1,12 @@
+import random
+
+def randInt(min=0, max=100):
+    diff = max - min
+    num = random.random()* diff + min
+    num = round(num)
+    return num
+
+print(randInt()) 		    # should print a random integer between 0 to 100
+print(randInt(max=50)) 	    # should print a random integer between 0 to 50
+print(randInt(min=50)) 	    # should print a random integer between 50 to 100
+print(randInt(min=50, max=500))    # should print a random integer between 50 and 500
